49eceb061577a9f340c726caf969b08c6b9fe7f8
EdgarMon <edgar.montenegro.mt@correo.usa.edu.co>
2021-12-06 04:44:26 -0500

    fird

---
+++ js/experienciajs.js
@@ -0,0 +1,27 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main (){
+    $('.menu_bar').click(function(){
+        if (contador ==1){
+            $('nav').animate({
+                top:'58.8px'
+                //right:'0%'
+                
+            });
+            contador = 0;
+        } else{
+            contador = 1;
+            $('nav').animate({
+                top:'-250%'
+                //right:'-100%'
+                
+            });
+        }
+    });
+}
+// Rating Initialization
+$(document).ready(function() {
+    $('#rateMe1').mdbRate();
+  });
